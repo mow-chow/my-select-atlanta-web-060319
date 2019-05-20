af64a906da4da7e85cf7eccb4f147d35881dc78b
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 20:25:15 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(nums)
-i = 0
-array = []
+  i = 0
+  array = []
 
-while i < nums.size
-  if yield(nums[i]) == true
-  array.push(nums[i])
-end
-  i+=1
-end
-array
+  while i < nums.size
+    if yield(nums[i]) == true
+      array.push(nums[i])
+    end
+    i += 1
+  end
+  array
 end
