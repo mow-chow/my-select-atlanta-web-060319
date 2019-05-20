edd86a730284f6c209a24697b0722931fc3fcfcf
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 20:25:25 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(nums)
+  i = 0
+  array = []
+
+  while i < nums.size
+    if yield(nums[i]) == true
+      array.push(nums[i])
+    end
+    i += 1
+  end
+  array
 end
