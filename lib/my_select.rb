8b9f5cb0a4848553e4777f3721c791507baf9363
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 20:03:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
-def my_select(select)
+def my_select(selection)
   i = 0
   array = []
-  while i < collection.length
+  while i < selection.length
     block_given?
-    array << yield(collection[i])
+    array << yield(selection[i])
     i += 1
   end
   array
